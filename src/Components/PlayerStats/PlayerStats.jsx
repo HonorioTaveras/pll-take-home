7f8e8869e8fbc6a04d98eaf8414701788637dce3
Honorio <htaveras90@gmail.com>
2021-02-10 23:45:57 -0700

    abstracted percentage handling into a helper function

--- src/Components/PlayerStats/PlayerStats.jsx
+++ src/Components/PlayerStats/PlayerStats.jsx
@@ -21,87 +21,85 @@ const PlayerStats = ({
     numPenalties,
     PIM,
   },
-}) => (
-  <div className='player-stats-container'>
-    <div className='row-1'>
-      <div className='row-1-col'>
-        <div className='title'>Points</div>
-        <div className='content'>{points}</div>
-      </div>
-      <div className='row-1-col'>
-        <div className='title'>1 Pt Goals</div>
-        <div className='content'>{onePointGoals}</div>
-      </div>
-      <div className='row-1-col'>
-        <div className='title'>2 Pt Goals</div>
-        <div className='content'>{twoPointGoals}</div>
-      </div>
-      <div className='row-1-col'>
-        <div className='title'>Assists</div>
-        <div className='content'>{assists}</div>
-      </div>
-      <div className='row-1-col'>
-        <div className='title'>Turnovers</div>
-        <div className='content'>{turnovers}</div>
-      </div>
-    </div>
-    <div className='row-2'>
-      <div className='row-2-col'>
-        <div className='title'>Shots</div>
-        <div className='content'>{shots}</div>
-      </div>
-      <div className='row-2-col'>
-        <div className='title'>Shot %</div>
-        <div className='content'>
-          {shotPct > 0 ? `${Math.round(shotPct * 100)}%` : 0}
+}) => {
+  const handlePercents = (value) =>
+    value > 0 ? `${Math.round(value * 100)}%` : 0;
+  return (
+    <div className='player-stats-container'>
+      <div className='row-1'>
+        <div className='row-1-col'>
+          <div className='title'>Points</div>
+          <div className='content'>{points}</div>
+        </div>
+        <div className='row-1-col'>
+          <div className='title'>1 Pt Goals</div>
+          <div className='content'>{onePointGoals}</div>
+        </div>
+        <div className='row-1-col'>
+          <div className='title'>2 Pt Goals</div>
+          <div className='content'>{twoPointGoals}</div>
+        </div>
+        <div className='row-1-col'>
+          <div className='title'>Assists</div>
+          <div className='content'>{assists}</div>
+        </div>
+        <div className='row-1-col'>
+          <div className='title'>Turnovers</div>
+          <div className='content'>{turnovers}</div>
         </div>
       </div>
-      <div className='row-2-col'>
-        <div className='title'>SOG</div>
-        <div className='content'>{shotsOnGoal}</div>
-      </div>
-      <div className='row-2-col'>
-        <div className='title'>SOG %</div>
-        <div className='content'>
-          {shotsOnGoalPct > 0 ? `${Math.round(shotsOnGoalPct * 100)}%` : 0}
+      <div className='row-2'>
+        <div className='row-2-col'>
+          <div className='title'>Shots</div>
+          <div className='content'>{shots}</div>
+        </div>
+        <div className='row-2-col'>
+          <div className='title'>Shot %</div>
+          <div className='content'>{handlePercents(shotPct)}</div>
+        </div>
+        <div className='row-2-col'>
+          <div className='title'>SOG</div>
+          <div className='content'>{shotsOnGoal}</div>
+        </div>
+        <div className='row-2-col'>
+          <div className='title'>SOG %</div>
+          <div className='content'>{handlePercents(shotsOnGoalPct)}</div>
+        </div>
+        <div className='row-2-col'>
+          <div className='title'>2 PT Sh</div>
+          <div className='content'>{twoPointShots}</div>
+        </div>
+        <div className='row-2-col'>
+          <div className='title'>2 Pt SOG</div>
+          <div className='content'>{twoPointShotsOnGoal}</div>
+        </div>
+        <div className='row-2-col'>
+          <div className='title'>2 Pt SOG %</div>
+          <div className='content'>
+            {handlePercents(twoPointShotsOnGoalPct)}
+          </div>
         </div>
       </div>
-      <div className='row-2-col'>
-        <div className='title'>2 PT Sh</div>
-        <div className='content'>{twoPointShots}</div>
-      </div>
-      <div className='row-2-col'>
-        <div className='title'>2 Pt SOG</div>
-        <div className='content'>{twoPointShotsOnGoal}</div>
-      </div>
-      <div className='row-2-col'>
-        <div className='title'>2 Pt SOG %</div>
-        <div className='content'>
-          {twoPointShotsOnGoalPct > 0
-            ? `${Math.round(twoPointShotsOnGoalPct * 100)}%`
-            : 0}
+      <div className='row-3'>
+        <div className='row-3-col'>
+          <div className='title'>GB</div>
+          <div className='content'>{groundBalls}</div>
+        </div>
+        <div className='row-3-col'>
+          <div className='title'>CT</div>
+          <div className='content'>{causedTurnovers}</div>
+        </div>
+        <div className='row-3-col'>
+          <div className='title'>Pen</div>
+          <div className='content'>{numPenalties}</div>
+        </div>
+        <div className='row-3-col'>
+          <div className='title'>PIM</div>
+          <div className='content'>{PIM}</div>
         </div>
       </div>
     </div>
-    <div className='row-3'>
-      <div className='row-3-col'>
-        <div className='title'>GB</div>
-        <div className='content'>{groundBalls}</div>
-      </div>
-      <div className='row-3-col'>
-        <div className='title'>CT</div>
-        <div className='content'>{causedTurnovers}</div>
-      </div>
-      <div className='row-3-col'>
-        <div className='title'>Pen</div>
-        <div className='content'>{numPenalties}</div>
-      </div>
-      <div className='row-3-col'>
-        <div className='title'>PIM</div>
-        <div className='content'>{PIM}</div>
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default PlayerStats;
