23e40145f39bdcbf5a02d097d335b0c8946defb9
Honorio <htaveras90@gmail.com>
2021-02-10 22:56:34 -0700

    rendering values for player stats

--- src/Components/PlayerStats/PlayerStats.jsx
+++ src/Components/PlayerStats/PlayerStats.jsx
@@ -25,72 +25,72 @@ const PlayerStats = ({
   <div className='player-stats-container'>
     <div className='row-1'>
       <div className='row-1-col'>
-        <div className='title'></div>
-        <div className='content'></div>
+        <div className='title'>Points</div>
+        <div className='content'>{points}</div>
       </div>
       <div className='row-1-col'>
-        <div className='title'></div>
-        <div className='content'></div>
+        <div className='title'>1 Pt Goals</div>
+        <div className='content'>{onePointGoals}</div>
       </div>
       <div className='row-1-col'>
-        <div className='title'></div>
-        <div className='content'></div>
+        <div className='title'>2 Pt Goals</div>
+        <div className='content'>{twoPointGoals}</div>
       </div>
       <div className='row-1-col'>
-        <div className='title'></div>
-        <div className='content'></div>
+        <div className='title'>Assists</div>
+        <div className='content'>{assists}</div>
       </div>
       <div className='row-1-col'>
-        <div className='title'></div>
-        <div className='content'></div>
+        <div className='title'>Turnovers</div>
+        <div className='content'>{turnovers}</div>
       </div>
     </div>
     <div className='row-2'>
       <div className='row-2-col'>
-        <div className='title'></div>
-        <div className='content'></div>
+        <div className='title'>Shots</div>
+        <div className='content'>{shots}</div>
       </div>
       <div className='row-2-col'>
-        <div className='title'></div>
-        <div className='content'></div>
+        <div className='title'>Shot %</div>
+        <div className='content'>{shotPct}</div>
       </div>
       <div className='row-2-col'>
-        <div className='title'></div>
-        <div className='content'></div>
+        <div className='title'>SOG</div>
+        <div className='content'>{shotsOnGoal}</div>
       </div>
       <div className='row-2-col'>
-        <div className='title'></div>
-        <div className='content'></div>
+        <div className='title'>SOG %</div>
+        <div className='content'>{shotsOnGoalPct}</div>
       </div>
       <div className='row-2-col'>
-        <div className='title'></div>
-        <div className='content'></div>
+        <div className='title'>2 PT Sh</div>
+        <div className='content'>{twoPointShots}</div>
       </div>
       <div className='row-2-col'>
-        <div className='title'></div>
-        <div className='content'></div>
+        <div className='title'>2 Pt SOG</div>
+        <div className='content'>{twoPointShotsOnGoal}</div>
       </div>
       <div className='row-2-col'>
-        <div className='title'></div>
-        <div className='content'></div>
+        <div className='title'>2 Pt SOG %</div>
+        <div className='content'>{twoPointShotsOnGoalPct}</div>
       </div>
     </div>
     <div className='row-3'>
       <div className='row-3-col'>
-        <div className='title'></div>
-        <div className='content'></div>
+        <div className='title'>GB</div>
+        <div className='content'>{groundBalls}</div>
       </div>
       <div className='row-3-col'>
-        <div className='title'></div>
-        <div className='content'></div>
+        <div className='title'>CT</div>
+        <div className='content'>{causedTurnovers}</div>
       </div>
       <div className='row-3-col'>
-        <div className='title'></div>
-        <div className='content'></div>
+        <div className='title'>Pen</div>
+        <div className='content'>{numPenalties}</div>
       </div>
       <div className='row-3-col'>
-        <div className='title'></div>
-        <div className='content'></div>
+        <div className='title'>PIM</div>
+        <div className='content'>{PIM}</div>
       </div>
     </div>
   </div>
